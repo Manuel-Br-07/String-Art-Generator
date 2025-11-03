4fd877f8cd5dd370e3ee7b98d644aadcc2d47f9c
Manuel-Br-07 <m.brokamp@lau365.de>
2025-11-03 15:28:34 +0000

    Speicherung der Linien in Liste

--- Main.java
+++ Main.java
@@ -18,7 +18,7 @@ public class Main {
     private StringArtPlotter stringArtPlotter = new StringArtPlotter();
 
     // Queue für Linienreihenfolge
-    private Queue<int[]> lineOrder = new Queue<>();
+    private Queue<int[]> lineOrder = new Queue();
 
     // ---------------- Bild einlesen ----------------
     public void immageToArray(String DateiName) {
@@ -44,15 +44,12 @@ public class Main {
                     int b = rgb & 0xFF;
 
                     // 5. Grau berechnen (Durchschnitt oder Luminanz)
-                    double gray = (0.299*r + 0.587*g + 0.114*b);
+                    double gray = (0.299 * r + 0.587 * g + 0.114 * b);
                     // System.out.println("" + gray);
 
-                    if(gray > 255)
-                    {
+                    if (gray > 255) {
                         gray = 255;
-                    }
-                    else if(gray < 0)
-                    {
+                    } else if (gray < 0) {
                         gray = 0;
                     }
 
@@ -96,22 +93,15 @@ public class Main {
 
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                if(bildArray[y][x]>=0)
-                {
-                    if(bildArray[y][x]> 0.5)
-                    {
+                if (bildArray[y][x] >= 0) {
+                    if (bildArray[y][x] > 0.5) {
                         System.out.print(" ");
-                    }
-                    else
-                    {
+                    } else {
                         System.out.print("█");
                     }
-                }
-                else
-                {
+                } else {
                     System.out.print("X");
-                    if(bildArray[y][x]>=0)
-                    {
+                    if (bildArray[y][x] >= 0) {
                         System.out.print("T");
                     }
                 }
@@ -124,8 +114,7 @@ public class Main {
     }
 
     // ---------------- Nägel ----------------
-    public void nailPositions(int pNails)
-    {
+    public void nailPositions(int pNails) {
         nails = pNails;
 
         nailCoords = new double[nails][2];
@@ -138,27 +127,26 @@ public class Main {
             System.out.println("" + i + "X" + nailCoords[i][0]);
             System.out.println("" + i + "Y" + nailCoords[i][1]);
 
-            if(i >= 1)
-            {
-                StringArtPlotter.addLine(nailCoords[i-1][0], nailCoords[i-1][1], nailCoords[i][0], nailCoords[i][1]);
-                System.out.println("xmin " + nailCoords[i-1][0] + " ymin " + nailCoords[i-1][1] + " xmax " + nailCoords[i][0] + " ymax " + nailCoords[i][1]);
+            if (i >= 1) {
+                StringArtPlotter.addLine(nailCoords[i - 1][0], nailCoords[i - 1][1], nailCoords[i][0],
+                        nailCoords[i][1]);
+                System.out.println("xmin " + nailCoords[i - 1][0] + " ymin " + nailCoords[i - 1][1] + " xmax "
+                        + nailCoords[i][0] + " ymax " + nailCoords[i][1]);
             }
         }
     }
 
-    public int stringArtGenerator()
-    {
+    public int stringArtGenerator() {
         int startNail = 0;
         int iterations = 0;
+        int[] linePos;
         double endpoint = averageColour() + (1 - averageColour()) / 10 * 9.1;
         System.out.println("Endpoint " + endpoint);
 
-        while(averageColour() <= endpoint )
-        {
+        while (averageColour() <= endpoint) {
             double bestScore = 0;
             int bestEndNail = -1;
-            for (int endNail = 0; endNail < nails; endNail++)
-            {
+            for (int endNail = 0; endNail < nails; endNail++) {
                 if (endNail != startNail)// gleiche Nagel überspringen
                 {
                     // Berechne Score für Linie von startNail zu endNail
@@ -174,38 +162,35 @@ public class Main {
                 }
             }
 
-            // System.out.println("xmin " + nailCoords[startNail][0] + " ymin " + nailCoords[startNail][1] + " xmax " + nailCoords[bestEndNail][0] + " ymax " + nailCoords[bestEndNail][1]);
+            // System.out.println("xmin " + nailCoords[startNail][0] + " ymin " +
+            // nailCoords[startNail][1] + " xmax " + nailCoords[bestEndNail][0] + " ymax " +
+            // nailCoords[bestEndNail][1]);
             // System.out.println("Start " + startNail + " Ende " + bestEndNail);
-            if(bestEndNail != -1)
-            {
+            if (bestEndNail != -1) {
                 StringArtPlotter.addLine(nailCoords[startNail][0], nailCoords[startNail][1], nailCoords[bestEndNail][0], nailCoords[bestEndNail][1]);
                 StringArtPlotter.show();
 
+                linePos = new int[]{startNail, bestEndNail};
+                lineOrder.enqueue(linePos);
+
                 lightenLine(startNail, bestEndNail, 0.2);
                 startNail = bestEndNail;
-                iterations ++;
-                System.out.println("iterations " + iterations + " average " + averageColour() );
+                iterations++;
+                System.out.println("iterations " + iterations + " average " + averageColour());
             }
         }
 
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                if(bildArray[y][x]>=0)
-                {
-                    if(bildArray[y][x]> 0.5)
-                    {
+                if (bildArray[y][x] >= 0) {
+                    if (bildArray[y][x] > 0.5) {
                         System.out.print(" ");
-                    }
-                    else
-                    {
+                    } else {
                         System.out.print("█");
                     }
-                }
-                else
-                {
+                } else {
                     System.out.print("X");
-                    if(bildArray[y][x]>=0)
-                    {
+                    if (bildArray[y][x] >= 0) {
                         System.out.print("T");
                     }
                 }
@@ -215,18 +200,17 @@ public class Main {
         return iterations;
     }
 
-    public double calculateLineScore(int startNail, int endNail)
-    {
+    public double calculateLineScore(int startNail, int endNail) {
         // Reelle Start- und Endkoordinaten
         double startX = nailCoords[startNail][0];
         double startY = nailCoords[startNail][1];
-        double endX   = nailCoords[endNail][0];
-        double endY   = nailCoords[endNail][1];
+        double endX = nailCoords[endNail][0];
+        double endY = nailCoords[endNail][1];
 
         double dx = endX - startX;
         double dy = endY - startY;
 
-        int steps = (int)Math.max(Math.abs(dx), Math.abs(dy));
+        int steps = (int) Math.max(Math.abs(dx), Math.abs(dy));
         double xIncrement = dx / steps;
         double yIncrement = dy / steps;
 
@@ -237,8 +221,8 @@ public class Main {
         int countedPoints = 0;
 
         for (int i = 0; i <= steps; i++) {
-            int px = (int)Math.round(x);
-            int py = (int)Math.round(y);
+            int px = (int) Math.round(x);
+            int py = (int) Math.round(y);
 
             if (px >= 0 && py >= 0 && px < bildArray[0].length && py < bildArray.length) {
                 score += 1.0 - bildArray[py][px];
@@ -252,18 +236,17 @@ public class Main {
         return countedPoints > 0 ? score / countedPoints : 0.0;
     }
 
-    public void lightenLine(int startNail, int endNail, double increment)
-    {
+    public void lightenLine(int startNail, int endNail, double increment) {
         // Reelle Start- und Endkoordinaten
         double startX = nailCoords[startNail][0];
         double startY = nailCoords[startNail][1];
-        double endX   = nailCoords[endNail][0];
-        double endY   = nailCoords[endNail][1];
+        double endX = nailCoords[endNail][0];
+        double endY = nailCoords[endNail][1];
 
         double dx = endX - startX;
         double dy = endY - startY;
 
-        int steps = (int)Math.max(Math.abs(dx), Math.abs(dy));
+        int steps = (int) Math.max(Math.abs(dx), Math.abs(dy));
         double xIncrement = dx / steps;
         double yIncrement = dy / steps;
 
@@ -271,13 +254,12 @@ public class Main {
         double y = startY;
 
         for (int i = 0; i <= steps; i++) {
-            int px = (int)Math.round(x);
-            int py = (int)Math.round(y);
+            int px = (int) Math.round(x);
+            int py = (int) Math.round(y);
 
             if (px >= 0 && py >= 0 && px < bildArray[0].length && py < bildArray.length) {
                 bildArray[py][px] += increment;
-                if(bildArray[py][px] > 1)
-                {
+                if (bildArray[py][px] > 1) {
                     bildArray[py][px] = 1;
                 }
             }
@@ -287,26 +269,23 @@ public class Main {
         }
 
     }
-    
-    public double averageColour()
-    {
+
+    public double averageColour() {
         double summ = 0;
         double ans = 0;
-        
+
         for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) 
-            {
+            for (int x = 0; x < width; x++) {
                 if (bildArray[y][x] != -1) {
                     summ = summ + bildArray[y][x];
-                    ans ++;
+                    ans++;
                 }
             }
         }
         return summ / ans;
     }
 
-    public void setDiameter(int pDiameter)
-    {
+    public void setDiameter(int pDiameter) {
         diameter = pDiameter;
     }
 }
